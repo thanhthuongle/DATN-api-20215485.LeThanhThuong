e5a6cc581ab407f370716f2c381f3c6c23c58e4f
thanhthuongle <thuong.lt215485@sis.hust.edu.vn>
2025-06-22 06:37:55 +0700

    edit mongodb address for agenda

--- src/agenda/agenda.js
+++ src/agenda/agenda.js
@@ -1,9 +1,31 @@
 const Agenda = require('agenda')
-import {env} from '~/config/environment'
+import { env } from '~/config/environment'
+
+const getMongoDbAddress = () => {
+  let mongodbAddress = ''
+
+  if (env.BUILD_MODE === 'dev') {
+    mongodbAddress = env.MONGODB_URI.endsWith('/')
+      ? env.MONGODB_URI + env.DATABASE_NAME
+      : `${env.MONGODB_URI}/${env.DATABASE_NAME}`
+  } else if (env.BUILD_MODE == 'production') {
+    const [mainPart, query] = env.MONGODB_URI.split('?')
+
+    const base = mainPart.endsWith('/')
+      ? mainPart.slice(0, -1)
+      : mainPart
+
+    mongodbAddress = query
+      ? `${base}/${env.DATABASE_NAME}?${query}`
+      : `${base}/${env.DATABASE_NAME}`
+  }
+
+  return mongodbAddress
+}
 
 export const agenda = new Agenda({
   db: {
-    address: env.MONGODB_URI+env.DATABASE_NAME,
+    address: getMongoDbAddress(),
     collection: 'system_tasks'
   },
   processEvery: '30 seconds'
